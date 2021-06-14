1a634b2206865c9cdc2737bb23a9c8dee0601ceb
Duncan Champney <DChampney@wareto.com>
2021-06-14 12:58:11 -0400

    Initial Commit

---
+++ FireEmitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireEmitter
+//
+//  Created by Duncan Champney on 6/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
